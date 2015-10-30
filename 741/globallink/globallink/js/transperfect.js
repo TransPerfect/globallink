bddb5f4d3213b38ce76edbb3fcb8d0ae217f0748
Jack Stone <jack@jackstonedev.com>
2015-10-30 15:54:41 -0400

    Remove unneeded comments.

--- 741/globallink/globallink/js/transperfect.js
+++ 741/globallink/globallink/js/transperfect.js
@@ -1,17 +1,14 @@
 (function($) {
   Drupal.behaviors.transperfect = {
     attach: function (context, settings) {
-      // Check to make sure that we're working with the right form.
       if ($('#transperfect-node-active-select-form', context).length == 0) {
         return;
       }
 
-      // Only run on page load.
       if (context != '#document') {
         return;
       }
 
-      // Binds change event on select element and submits form.
       $('#edit-submission', context).change(function() {
         $('#transperfect-node-active-select-form').submit();
       });
