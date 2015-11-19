572367a84e598ac37d3d62df0a1b6b9b3b8751e7
Jack Stone <jack@jackstonedev.com>
2015-11-19 15:12:40 -0500

    Address Issues 6, 15, and 16 in JavaScript files.

--- 741/globallink/globallink/transperfect_block/js/transperfect_block.js
+++ 741/globallink/globallink/transperfect_block/js/transperfect_block.js
@@ -1,67 +1,77 @@
 var _div;
 var _img;
-(function ($) {
-    Drupal.behaviors.transperfect =   {
-        attach: function() {
-            if(Drupal.settings.transperfect != undefined) {
-                var popup = Drupal.settings.transperfect.popup;
-                var previewpath = Drupal.settings.transperfect.previewpath;
-                var rids = Drupal.settings.transperfect.rids;
-                _img = Drupal.settings.transperfect.ajax_image;
-                $.each(popup, function(link, div) {
-                    //Attach click event and set the contents of div
-                    $('#' + link).click(function() {
-                        _div = div;
-                        $('#' + div).dialog({
-                            modal: true,
-                            show: {
-                                effect: "blind",
-                                duration: 100
-                            },
-                            width: 700,
-                            height : 400
-                        });
-                        $('#' + div).empty();
-                        $('#' + div).append('<div style="width: 100%; height:100%; text-align:center;"><div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div></div>');
-                        $.ajax({
-                            type: 'POST',
-                            url: previewpath,
-                            dataType: 'json',
-                            data: 'rid=' + rids[div],
-                            success: ajax_completed,
-                            error: function(xhr, textStatus, errorThrown) {
-                                $('#' + _div).empty();
-                                $('#'+div).html(textStatus);
-                            }
-                        });
-                    });
-                });
+
+(function($) {
+  Drupal.behaviors.transperfect = {
+    attach: function() {
+      if (Drupal.settings.transperfect == undefined) {
+        return;
+      }
+
+      var popup = Drupal.settings.transperfect.popup;
+      var previewpath = Drupal.settings.transperfect.previewpath;
+      var rids = Drupal.settings.transperfect.rids;
+
+      _img = Drupal.settings.transperfect.ajax_image;
+
+      $.each(popup, function(link, div) {
+        $('#' + link).click(function() {
+          _div = div;
+
+          $('#' + div).dialog({
+            modal: true,
+            show: {
+              effect: "blind",
+              duration: 100
+            },
+            width: 700,
+            height : 400
+          });
+
+          $('#' + div).empty();
+
+          $('#' + div).append('<div style="width: 100%; height:100%; text-align:center;"><div class="ajax-progress ajax-progress-throbber"><div class="throbber">&nbsp;</div></div></div>');
+
+          $.ajax({
+            type: 'POST',
+            url: previewpath,
+            dataType: 'json',
+            data: 'rid=' + rids[div],
+            success: ajax_completed,
+            error: function(xhr, textStatus, errorThrown) {
+              $('#' + _div).empty();
+              $('#'+div).html(textStatus);
             }
-        }
+          });
+        });
+      });
     }
+  }
 })(jQuery);
 
-function escape_html(string)
-{
+function escape_html(string) {
   return jQuery('<pre>').text(string).html();
 }
 
 function ajax_completed(data) {
-  var content = '<TABLE class="tpt_popup_table"><TR><TH>&nbsp;</TH><TH>Source Content</TH><TH>Translated Content</TH></TR>';
+  var content = '<table class="tpt_popup_table"><tr><th>&nbsp;</th><th>Source Content</th><th>Translated Content</th></tr>';
+
   error = data['error'];
   target = data['target'];
   source_obj = data['source'];
 
   if (error != null && error != undefined) {
-      content += '<TR><TD colspan="3"><span style="color: red;text-align: center;">' + error + '</span></TD></TR>';
-      content += '</TABLE>';
-      jQuery('#' + _div).empty();
-      jQuery('#' + _div).append(content);
-      return true;
+    content += '<tr><td colspan="3"><span style="color: red;text-align: center;">' + error + '</span></td></tr>';
+    content += '</table>';
+
+    jQuery('#' + _div).empty();
+    jQuery('#' + _div).append(content);
+
+    return true;
   }
 
   if (source_obj == null || source_obj == undefined || source_obj == '') {
-      return true;
+    return true;
   }
 
   jQuery.each(target, function(field, f_object) {
@@ -86,7 +96,7 @@ function ajax_completed(data) {
               label = 'Title';
             }
 
-            content += '<TR><TD><b>' + label + '</b></TD><TD>' + source_text + '</TD><TD>' + target_text + '</TD></TR>';
+            content += '<tr><td><b>' + label + '</b></td><td>' + source_text + '</td><td>' + target_text + '</td></tr>';
           }
         }
 
@@ -96,7 +106,7 @@ function ajax_completed(data) {
         var target_text = '';
 
         if (source_obj[field] != null && source_obj[field] != undefined) {
-            source_text = escape_html(source_obj[field]);
+          source_text = escape_html(source_obj[field]);
         }
 
         if (source_text == '') {
@@ -111,7 +121,7 @@ function ajax_completed(data) {
               label = 'Body';
             }
 
-            content += '<TR><TD><b>' + label + '</b></TD><TD>' + source_text + '</TD><TD>' + target_text + '</TD></TR>';
+            content += '<tr><td><b>' + label + '</b></td><td>' + source_text + '</td><td>' + target_text + '</td></tr>';
           }
         }
 
@@ -119,7 +129,8 @@ function ajax_completed(data) {
     }
   });
 
-  content += '</TABLE>';
+  content += '</table>';
+
   jQuery('#' + _div).empty();
   jQuery('#' + _div).append(content);
 
--- 741/globallink/globallink/transperfect_entity/js/transperfect_entity_active_submissions.js
+++ 741/globallink/globallink/transperfect_entity/js/transperfect_entity_active_submissions.js
@@ -1,6 +1,6 @@
 (function($) {
   Drupal.behaviors.transperfectEntityActiveSubmissions = {
-    attach: function (context, settings) {
+    attach: function(context, settings) {
       if ($('.transperfect-entity-active-select-form', context).length == 0) {
         return;
       }
