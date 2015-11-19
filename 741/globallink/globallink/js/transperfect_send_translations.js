07da89b060c8b1a27f66e35825e47ae83372bc19
Jack Stone <jack@jackstonedev.com>
2015-11-19 15:27:54 -0500

    Continue to fix Issue 23.

---
+++ 741/globallink/globallink/js/transperfect_send_translations.js
@@ -0,0 +1,15 @@
+(function($) {
+  Drupal.behaviors.transperfectSendTranslations = {
+    attach: function (context, settings) {
+      if ($('.transperfect-node-form-clear', context).length == 0) {
+        return;
+      }
+
+      $('.transperfect-node-form-clear-submit', context).click(function(e) {
+        if (!confirm("Are you sure you want to clear the changed status for the selected content(s)?")) {
+          e.preventDefault();
+        }
+      });
+    }
+  };
+})(jQuery);
